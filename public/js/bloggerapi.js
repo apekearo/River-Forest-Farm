d2fc0379994bd0e82acd264d00a6a1d95b35c8a6
apekearo <apekearo@gmail.com>
2018-11-16 11:27:49 -0600

    in the middle of making an auth0 file, added to top button in the dataChart, added shading to header bar depending on active page, hoping the crop data uploads to mysql this time

--- public/js/bloggerapi.js
+++ public/js/bloggerapi.js
@@ -1,3 +1,6 @@
 const hahah = 'AIzaSyD-QCuU9uy7JaFSwq6mU9vNQFMSI8oSNLU';
 //greyed out one is alberts key  the one in use is Devah's
 // const hahah = 'AIzaSyB0sUGAhtwl84k7_1lnKdYjcEGjjAOByeU';
+
+//this is my sess={ secret} for authO 
+const secret = 'riverforestfarm'
