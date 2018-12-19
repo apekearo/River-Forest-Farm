3563db08cd6e28a1d29efd8792d977fa456efe04
apekearo <apekearo@gmail.com>
2018-12-19 13:36:00 -0600

    added favicon, added ken testimonial, added goat video to back of blog.html, testing how the new twitch stream looks on cell phone mode in cell phone version

--- public/js/app.js
+++ public/js/app.js
@@ -30,7 +30,7 @@ var strategy = new Auth0Strategy(
     clientID: process.env.AUTH0_CLIENT_ID,
     clientSecret: process.env.AUTH0_CLIENT_SECRET,
     callbackURL:
-      process.env.AUTH0_CALLBACK_URL || 'http://localhost:8080/blog.html'
+      process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000'
   },
   function (accessToken, refreshToken, extraParams, profile, done) {
     // accessToken is the token to call Auth0 API (not needed in the most cases)
@@ -55,3 +55,17 @@ passport.serializeUser(function (user, done) {
   passport.deserializeUser(function (user, done) {
     done(null, user);
   });
+
+  // app.js
+
+var userInViews = require('./lib/middleware/userInViews');
+var authRouter = require('./routes/auth');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+// ..
+app.use(userInViews());
+app.use('/', authRouter);
+app.use('/', indexRouter);
+app.use('/', usersRouter);
+// ..
